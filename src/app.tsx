d38f2b996575a2dd974eb5e1d5bac0a14edc076d
chenxingyu0830 <chenxingyu0830@gmail.com>
2021-12-30 21:40:41 +0800

    Fear: add home page

---
+++ src/app.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Routes, HashRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Home } from './pages/home';
+import { Config } from './pages/config';
+
+function App() {
+
+    return (
+        <div className="App">
+            <HashRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/config" element={<Config />} />
+                </Routes>
+            </HashRouter>
+        </div>
+    );
+}
+
+export default App;
