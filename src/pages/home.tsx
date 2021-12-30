7cb8b9185d0015cce8a8552f514734c31ad896a3
chenxingyu0830 <chenxingyu0830@gmail.com>
2021-12-30 22:34:30 +0800

    Feat: add tailwindcss

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 export const Home = () => {
-  return (
-    <div>
-      你好
-    </div>
-  );
+    return (
+        <div className="text-7xl border-2 border-red-200">
+            你好
+        </div>
+    );
 };
