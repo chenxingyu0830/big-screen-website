9520aa52597df48993fa30b9c80c63687264fad4
chenxingyu0830 <chenxingyu0830@gmail.com>
2022-06-20 02:26:44 +0800

    feat: header - 引入百度地图，左上角获取IP定位

---
+++ src/pages/header/header.tsx
@@ -0,0 +1,52 @@
+import dayjs from "dayjs";
+import { features } from "process";
+import { useEffect, useState } from "react";
+import PositionSvg from "../../images/position.svg";
+
+const Header = () => {
+  const [date, setDate] = useState(dayjs());
+  const [cityName, setCityName] = useState("");
+
+  useEffect(() => {
+    getLocationBaiduIp();
+  }, [])
+
+  useEffect(() => {
+    setTimeout(() => {
+      setDate(dayjs());
+    }, 1000);
+  }, [date]);
+
+  /*获取用户当前位置（ip定位）*/
+  function getLocationBaiduIp() {
+    function myFun(result) {
+      const cityName = result.name;
+      console.log("result", result) // {center: O, {lng: 120.161693, lat: 30.280059},code: 179,level: 12,name: "杭州市"}}
+      setCityName(cityName);
+    }
+    var myCity = new (BMap).LocalCity();
+    myCity.get(myFun);
+  }
+
+  return (
+    <header className="flex justify-between items-center text-blue-100 mx-10 text-5xl shadow-md h-20">
+      <div className="flex flex-1">
+        <img src={PositionSvg} className="h-10 mr-4" />
+        <span className="flex items-center text-2xl">{cityName}</span>
+      </div>
+      <span className="flex-1 align-middle text-center">
+        新冠疫情实况大屏
+      </span>
+      <div className="flex flex-1 flex-col text-2xl">
+        <div className="flex justify-end">
+          {date.format("HH:mm:ss")}
+        </div>
+        <div className="flex justify-end">
+          {date.format("dddd, MMMM D, YYYY")}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,8 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useMemo } from "react";
 import _ from "lodash";
-import dayjs from "dayjs";
 import useSWR from "swr";
-import PositionSvg from "../images/position.svg";
+import Header from "./header/header";
 import { fetcher } from "../util/rest";
-import "./home.scss";
 import { Chart1 } from "./chart/chrat-1";
 import { Chart2 } from "./chart/chrat-2";
 import { Chart3 } from "./chart/chart-3";
@@ -13,80 +11,59 @@ import { Chart6 } from "./chart/chart-6";
 import { Chart7 } from "./chart/chart-7";
 import { Chart4 } from "./chart/chart-4";
 
+import "./home.scss";
+
 export const Home = () => {
-    const [date, setDate] = useState(dayjs());
-    useEffect(() => {
-        setTimeout(() => {
-            setDate(dayjs());
-        }, 1000);
-    }, [date]);
+  const { data } = useSWR(
+    "https://lab.isaaclin.cn/nCoV/api/area?latest=1&sort=currentConfirmedCount",
+    fetcher,
+    {
+      refreshInterval: 60 * 60 * 1000,
+      suspense: true,
+    }
+  );
 
-    const { data } = useSWR(
-        "https://lab.isaaclin.cn/nCoV/api/area?latest=1&sort=currentConfirmedCount",
-        fetcher,
-        {
-            refreshInterval: 60 * 60 * 1000,
-            suspense: true,
-        }
-    );
+  const { data: data2 } = useSWR(
+    "https://lab.isaaclin.cn/nCoV/api/overall",
+    fetcher,
+    {
+      refreshInterval: 30 * 1000,
+      suspense: true,
+    }
+  );
 
-    const { data: data2 } = useSWR(
-        "https://lab.isaaclin.cn/nCoV/api/overall",
-        fetcher,
-        {
-            refreshInterval: 30 * 1000,
-            suspense: true,
-        }
-    );
-
-    return (
-        <div className="flex flex-col flex-1">
-            <header className="flex justify-between items-center text-blue-100 mx-10 text-5xl shadow-md h-20">
-                <div className="flex flex-1">
-                    <img src={PositionSvg} className="h-10 mr-4" />
-                    <span className="flex items-center text-2xl">Hangzhou</span>
-                </div>
-                <span className="flex-1 align-middle text-center">
-                    新冠疫情实况大屏
-                </span>
-                <div className="flex flex-1 flex-col text-2xl">
-                    <div className="flex justify-end">
-                        {date.format("HH:mm:ss")}
-                    </div>
-                    <div className="flex justify-end">
-                        {date.format("dddd, MMMM D, YYYY")}
-                    </div>
-                </div>
-            </header>
-            <main className="flex-1 gap-3">
-                <section className="section1">
-                    <div className="title">各省确诊人数统计</div>
-                    {useMemo(() => <Chart1 data={data} />, [data])}
-                </section>
-                <section className="section2">
-                    <div className="title">各省疑似感染人数统计</div>
-                    {useMemo(() => <Chart2 data={data} />, [data])}
-                </section>
-                <section className="section3">
-                    <div className="title">七大州确诊人数统计</div>
-                    {useMemo(() => <Chart3 data={data} />, [data])}
-                </section>
-                <section className="section4 ">
-                    {useMemo(() => <Chart4 data={data2} />, [data2])}
-                </section>
-                <section className="section5">
-                    <div className="title">浙江省各市确诊人数统计</div>
-                    {useMemo(() => <Chart5 data={data} />, [data])}
-                </section>
-                <section className="section6">
-                    <div className="title">杭州市各项数据统计</div>
-                    {useMemo(() => <Chart6 data={data} />, [data])}
-                </section>
-                <section className="section7">
-                    <div className="title">疫情主要国家确诊人数统计</div>
-                    {useMemo(() => <Chart7 data={data} />, [data])}
-                </section>
-            </main>
-        </div>
-    );
+  return (
+    <div className="flex flex-col flex-1">
+      <Header />
+      <main className="flex-1 gap-3">
+        <section className="section1">
+          <div className="title">各省确诊人数统计</div>
+          {useMemo(() => <Chart1 data={data} />, [data])}
+        </section>
+        <section className="section2">
+          <div className="title">各省疑似感染人数统计</div>
+          {useMemo(() => <Chart2 data={data} />, [data])}
+        </section>
+        <section className="section3">
+          <div className="title">七大州确诊人数统计</div>
+          {useMemo(() => <Chart3 data={data} />, [data])}
+        </section>
+        <section className="section4 ">
+          {useMemo(() => <Chart4 data={data2} />, [data2])}
+        </section>
+        <section className="section5">
+          <div className="title">浙江省各市确诊人数统计</div>
+          {useMemo(() => <Chart5 data={data} />, [data])}
+        </section>
+        <section className="section6">
+          <div className="title">杭州市各项数据统计</div>
+          {useMemo(() => <Chart6 data={data} />, [data])}
+        </section>
+        <section className="section7">
+          <div className="title">疫情主要国家确诊人数统计</div>
+          {useMemo(() => <Chart7 data={data} />, [data])}
+        </section>
+      </main>
+    </div>
+  );
 };
